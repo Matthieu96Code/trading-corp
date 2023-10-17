4a8b9de03ef2713b3c99ac87c8731324329e4e73
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-10-17 23:05:30 +0000

    Add interactions button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,6 +9,11 @@ export default function Header() {
           <p className="pt-20 text-white text-lg">Trade Synthetic Indices, Forex, and Cryptos with our top recommended brokers.</p>
           <p className="text-white text-lg text-lg">24/7</p>
           <p className="text-white text-4xl pt-10">It is free to create an account</p>
+          <button className="border-2 bg-red-500 text-white py-2 px-10 rounded-lg text-lg">Create a synthetic account</button>
+          <br />
+          <button className="border-2 bg-red-500 text-white py-2 px-10 rounded-lg text-lg">Create a Forex account</button>
+          <p className="text-white text-lg">New to trading? Take advantage of our free trading education modules.</p>
+          <button className="border-2 bg-red-500 text-white py-2 px-10 rounded-lg text-lg">Learn</button>
         </div>
     </header>
   )
