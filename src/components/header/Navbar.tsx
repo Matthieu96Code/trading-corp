fa7e56b2cdeda4975ad7287f06530b37c5ba0a63
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-10-17 15:06:35 +0000

    Spacing and bg for navbar

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -2,9 +2,9 @@ import Link from "../../../node_modules/next/link"
 
 export default function Navbar() {
   return (
-    <nav className="flex row justify-between mx-[10%] " >
+    <nav className="bg-gray-800 flex row justify-between px-[15%] py-6 text-white" >
       <h1>Trading<span>Corp</span></h1>
-      <ul className="flex row">
+      <ul className="flex row w-1/6 justify-between">
         <li>
             <Link href="#products">Products</Link>
         </li>
