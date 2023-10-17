6e0a120890cd16d4e8dd73ea1a076f02a19d9f15
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-10-17 23:03:49 +0000

    Add a background to the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,8 @@ import Navbar from "./header/Navbar";
 
 export default function Header() {
   return (
-    <header>
+    // <header className="h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+    <header className="h-screen bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
         <Navbar />
     </header>
   )
