464724b25df9da6c6b5748681f1e3ee82442036b
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-10-17 23:04:57 +0000

    Add Introduction text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,11 @@ export default function Header() {
     // <header className="h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
     <header className="h-screen bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
         <Navbar />
+        <div className="px-[8%]">
+          <p className="pt-20 text-white text-lg">Trade Synthetic Indices, Forex, and Cryptos with our top recommended brokers.</p>
+          <p className="text-white text-lg text-lg">24/7</p>
+          <p className="text-white text-4xl pt-10">It is free to create an account</p>
+        </div>
     </header>
   )
 }
