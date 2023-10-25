ed4694d028beddaa4db8fc9f0b3b5330eb82d1ad
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-10-25 14:13:32 +0000

    Add context and image for cash back V3

--- src/components/products/CashBackV3.tsx
+++ src/components/products/CashBackV3.tsx
@@ -1,12 +1,24 @@
+import Image from "../../../node_modules/next/image";
+import cashBackV3 from "../../../public/image/feature image f 0.png";
+
 export default function CashBackV3() {
     return (
         <div>
+            <h3>Cash Back v3</h3>
             <div>
-                <h3>Cash Back v3</h3>
+                <Image src={cashBackV3} alt="Cash Back v3 logo"/>
                 <div>
-                    <img src="" alt="Cash Back v3 logo"/>
                     <div>
-                        <h4></h4>
+                        <h4>To make money in trading, you don&apos;t need much.</h4>
+                        <p>Most traders believe that to be profitable in trading, it takes years of experience or memorizing every technical indicator and chart pattern. This belief is mistaken because all you really need is a solid trading system that supports and guides your decision-making in the markets.</p>
+                        <br />
+                        <p><span>Cash Back v3</span> addresses the profitability needs of traders. It&apos;s the first trading system that can anticipate market manipulations and assist traders in improving the quality of their trades.</p>
+
+                        <h4>How does it work?</h4>
+                        <p>This robot is installed on a PC and is used on the 1-hour time frame. The system analyzes the market and automatically takes positions with a precise signal quality of 95%. Additionally, it offers several features such as a trailing stop and an intelligent take profit controlled by artificial intelligence, allowing you to manage your risks more effectively.</p>
+
+                        <h4>Good to know!</h4>
+                        <p>Payments are currently made via Perfect Money. After your payment, you need to enter your email address to receive your software automatically by email. If you don&apos;t have Perfect Money, you can pay using Mobile Money by contacting the Trading Mate payment agent directly, exclusively via WhatsApp at +225 05 84 57 54 37. Please note that Trading Mate does not collect your email address. The email you provide is solely for sending you the software after payment.</p>
                     </div>
                 </div>
             </div>
