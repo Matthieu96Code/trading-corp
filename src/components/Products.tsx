b82ba7d976f565ead96b25097ffe4a9d0248c8bd
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-10-25 13:21:43 +0000

    Create cash back component

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,7 +1,12 @@
+import CashBackV3 from "./products/CashBackV3"
+
 export default function Products() {
     return (
         <section>
             <h2>Products</h2>
+            <div>
+                <CashBackV3 />
+            </div>
         </section>
     )
 }
---
+++ src/components/products/CashBackV3.tsx
@@ -0,0 +1,15 @@
+export default function CashBackV3() {
+    return (
+        <div>
+            <div>
+                <h3>Cash Back v3</h3>
+                <div>
+                    <img src="" alt="Cash Back v3 logo"/>
+                    <div>
+                        <h4></h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
