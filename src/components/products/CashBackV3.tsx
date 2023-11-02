796f16960df0c7eedd791fcd66c7c17ad339366a
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-11-02 16:43:04 +0000

    Update cash back v3 content

--- src/components/products/CashBackV3.tsx
+++ src/components/products/CashBackV3.tsx
@@ -9,9 +9,6 @@ export default function CashBackV3() {
                 <Image src={cashBackV3} alt="Cash Back v3 logo"/>
                 <div>
                     <div>
-                        <h4>To make money in trading, you don&apos;t need much.</h4>
-                        <p>Most traders believe that to be profitable in trading, it takes years of experience or memorizing every technical indicator and chart pattern. This belief is mistaken because all you really need is a solid trading system that supports and guides your decision-making in the markets.</p>
-                        <br />
                         <p><span>Cash Back v3</span> addresses the profitability needs of traders. It&apos;s the first trading system that can anticipate market manipulations and assist traders in improving the quality of their trades.</p>
 
                         <h4>How does it work?</h4>
@@ -19,6 +16,9 @@ export default function CashBackV3() {
 
                         <h4>Good to know!</h4>
                         <p>Payments are currently made via Perfect Money. After your payment, you need to enter your email address to receive your software automatically by email. If you don&apos;t have Perfect Money, you can pay using Mobile Money by contacting the Trading Mate payment agent directly, exclusively via WhatsApp at +225 05 84 57 54 37. Please note that Trading Mate does not collect your email address. The email you provide is solely for sending you the software after payment.</p>
+                        <p>Cash Back v3 Price</p>
+                        <p>85 USD</p>
+                        <button>Buy</button>
                     </div>
                 </div>
             </div>
