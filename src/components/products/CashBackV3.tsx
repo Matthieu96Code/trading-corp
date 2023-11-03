873ef47788223f192ed2aa09b618e501c51366a7
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-11-03 13:42:29 +0000

    Basic style on ca back V3 card

--- src/components/products/CashBackV3.tsx
+++ src/components/products/CashBackV3.tsx
@@ -3,22 +3,24 @@ import cashBackV3 from "../../../public/image/feature image f 0.png";
 
 export default function CashBackV3() {
     return (
-        <div>
-            <h3>Cash Back v3</h3>
-            <div>
-                <Image src={cashBackV3} alt="Cash Back v3 logo"/>
+        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+            <div className="md:flex">
+                <div className="md:shrink-0">
+                    <Image className="h-48 w-full object-cover md:h-full md:w-48" src={cashBackV3} alt="Cash Back v3 logo"/>
+                </div>
                 <div>
-                    <div>
-                        <p><span>Cash Back v3</span> addresses the profitability needs of traders. It&apos;s the first trading system that can anticipate market manipulations and assist traders in improving the quality of their trades.</p>
+                    <div className="p-8">
+                        <h3 className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Cash Back v3</h3>
+                        <p className="my-2 text-slate-500"><span>Cash Back v3</span> addresses the profitability needs of traders. It&apos;s the first trading system that can anticipate market manipulations and assist traders in improving the quality of their trades.</p>
 
-                        <h4>How does it work?</h4>
-                        <p>This robot is installed on a PC and is used on the 1-hour time frame. The system analyzes the market and automatically takes positions with a precise signal quality of 95%. Additionally, it offers several features such as a trailing stop and an intelligent take profit controlled by artificial intelligence, allowing you to manage your risks more effectively.</p>
+                        <h4 className="product-subtitle">How does it work?</h4>
+                        <p className="my-2 text-slate-500">This robot is installed on a PC and is used on the 1-hour time frame. The system analyzes the market and automatically takes positions with a precise signal quality of 95%. Additionally, it offers several features such as a trailing stop and an intelligent take profit controlled by artificial intelligence, allowing you to manage your risks more effectively.</p>
 
-                        <h4>Good to know!</h4>
-                        <p>Payments are currently made via Perfect Money. After your payment, you need to enter your email address to receive your software automatically by email. If you don&apos;t have Perfect Money, you can pay using Mobile Money by contacting the Trading Mate payment agent directly, exclusively via WhatsApp at +225 05 84 57 54 37. Please note that Trading Mate does not collect your email address. The email you provide is solely for sending you the software after payment.</p>
-                        <p>Cash Back v3 Price</p>
-                        <p>85 USD</p>
-                        <button>Buy</button>
+                        <h4 className="product-subtitle">Good to know!</h4>
+                        <p className="my-2 text-slate-500">Payments are currently made via Perfect Money. After your payment, you need to enter your email address to receive your software automatically by email. If you don&apos;t have Perfect Money, you can pay using Mobile Money by contacting the Trading Mate payment agent directly, exclusively via WhatsApp at +225 05 84 57 54 37. Please note that Trading Mate does not collect your email address. The email you provide is solely for sending you the software after payment.</p>
+                        <p  className="product-subtitle">Cash Back v3 Price</p>
+                        <p className="product-subtitle">85 USD</p>
+                        <button className="border-2 border-red-500 bg-red-500 text-white py-2 px-10 rounded-lg text-lg">Buy</button>
                     </div>
                 </div>
             </div>
