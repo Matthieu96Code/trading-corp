7a76bf254c09d590ea263bc47a9d096cfba28109
Matthieu96Code <senoumatthieu96@gmail.com>
2023-11-07 13:42:17 +0000

    Spacing and centered product text

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -3,11 +3,11 @@ import SmartMoneyV2 from "./products/SmartMoneyV2"
 
 export default function Products() {
     return (
-        <section className="py-10 flex flex-col">
+        <section className="bg-gray-100 py-10 flex flex-col">
             <h2 className="my-4 text-center text-4xl">Products</h2>
             <div className="my-4">
                 <h4 className="my-2 text-center text-2xl">To make money in trading, you don&apos;t need much.</h4>
-                <p className="text-center text">Most traders believe that to be profitable in trading, it takes years of experience or memorizing every technical indicator and chart pattern. This belief is mistaken because all you really need is a solid trading system that supports and guides your decision-making in the markets.</p>
+                <p className="text-center text px-2 md:px-24">Most traders believe that to be profitable in trading, it takes years of experience or memorizing every technical indicator and chart pattern. This belief is mistaken because all you really need is a solid trading system that supports and guides your decision-making in the markets.</p>
             </div>
             <div>
                 <CashBackV3 />
