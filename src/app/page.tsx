564748dddd64ee05cb3249803f78c8f2c7ba68e0
Matthieu96Code <senoumatthieu96@gmail.com>
2023-11-19 14:06:06 +0000

    Import faq component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
 import Products from "@/components/Products";
+import Faq from "@/components/Faq";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Products />
+      <Faq />
     </main>
   )
 }
