16ef7477e02a09dba2804a14b306f57c4d8f5c25
Matthieu96Code <senoumatthieu96@gmail.com>
2023-11-19 16:52:52 +0000

    Add content faq three

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -5,7 +5,7 @@ export default function Faq() {
             <ol className="w-11/12 m-auto">
                 <li className="py-3">
                     <h3 className="py-2 font-bold">
-                        <span>1)</span>On which platform do the Trading Systems/Robots operate?
+                        <span>1 ) </span>On which platform do the Trading Systems/Robots operate?
                     </h3>
                     <p>
                         The systems & robots are used on the MetaTrader 5 (MT5) platform.
@@ -13,10 +13,18 @@ export default function Faq() {
                 </li>
                 <li className="py-3">
                     <h3 className="py-2 font-bold">
-                        <span>2)</span>Can the Trading Systems/Robots work on a mobile phone?
+                        <span>2 ) </span>Can the Trading Systems/Robots work on a mobile phone?
                     </h3>
                     <p>
                         Although the systems/robots are optimized for computer use, you have the option to use a FOREX VPS (Virtual Private Server) to run them on your phone.
+                    </p>
+                </li>
+                <li className="py-3">
+                    <h3 className="py-2 font-bold">
+                        <span>3 ) </span>On which time frame do we trade with the Trading Systems/Robots?
+                    </h3>
+                    <p>
+                        Based on our experience with the systems/robots, it is recommended to trade on a 1-hour time frame (H1). However, you can test them on longer time frames, such as 4 hours (H4), 1 day (D1), 1 week (W1), 1 month (MN).
                     </p>
                 </li>
             </ol>
