4c20262c3de4e3c2e7ac74948c3ede422637d6af
Matthieu96Code <senoumatthieu96@gmail.com>
2023-11-19 16:11:40 +0000

    Add content faq one

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -3,6 +3,14 @@ export default function Faq() {
         <section className="bg-gray-100 py-2 flex flex-col">
             <h2 className="text-center text-4xl pb-5">FAQ</h2>
             <ul className="w-11/12 m-auto">
+                <ol className="py-3">
+                    <h3 className="py-2 font-bold">
+                        <span>1)</span>On which platform do the Trading Systems/Robots operate?
+                    </h3>
+                    <p>
+                        The systems & robots are used on the MetaTrader 5 (MT5) platform.
+                    </p>
+                </ol>
             </ul>
         </section>
     )
