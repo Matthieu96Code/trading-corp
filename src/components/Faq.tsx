f0cd21aa659e929b77aa37e332b830f8fb96a401
Matthieu96Code <senoumatthieu96@gmail.com>
2023-11-19 14:05:32 +0000

    Create faq component

---
+++ src/components/Faq.tsx
@@ -0,0 +1,9 @@
+export default function Faq() {
+    return(
+        <section className="bg-gray-100 py-2 flex flex-col">
+            <h2 className="text-center text-4xl pb-5">FAQ</h2>
+            <ul className="w-11/12 m-auto">
+            </ul>
+        </section>
+    )
+}
