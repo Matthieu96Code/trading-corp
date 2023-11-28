da62a38523390e36aa2897ff83344b2186972e5a
Matthieu96Code <senoumatthieu96@gmail.com>
2023-11-28 18:38:03 +0000

    Change product direction in row

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -6,15 +6,20 @@ import Trade from "./products/Trade"
 
 export default function Products() {
     return (
-        <section className="bg-gray-100 py-10 flex flex-col">
-            <h2 className="my-4 text-center text-4xl">Products & services</h2>
-            <div className="my-4">
-                <h4 className="my-2 text-center text-2xl">To make money in trading, you don&apos;t need much.</h4>
-                <p className="text-center text px-2 md:px-24">Most traders believe that to be profitable in trading, it takes years of experience or memorizing every technical indicator and chart pattern. This belief is mistaken because all you really need is a solid trading system that supports and guides your decision-making in the markets.</p>
+        <section className="flex flex-col">
+            <div className="py-10 ">
+                <h2 className="my-4 text-center text-4xl">Products & services</h2>
+                <div className="my-4">
+                    <h4 className="my-2 text-center text-2xl">To make money in trading, you don&apos;t need much.</h4>
+                    <p className="text-center text px-2 md:px-24">Most traders believe that to be profitable in trading, it takes years of experience or memorizing every technical indicator and chart pattern. This belief is mistaken because all you really need is a solid trading system that supports and guides your decision-making in the markets.</p>
+                </div>
+                <div className=" lg:flex lg:justify-around">
+                    <CashBackV3 />
+                    <SmartMoneyV2 />
+                </div>
             </div>
-            <div>
-                <CashBackV3 />
-                <SmartMoneyV2 />
+            <div  className="bg-gray-100">
+                <Trade />
             </div>
             <div className="my-4">
                 <h4 className="my-2 text-center text-2xl">Training</h4>
@@ -25,9 +30,6 @@ export default function Products() {
             </div>
             <div>
                 <Automate />
-            </div>
-            <div>
-                <Trade />
             </div>
         </section>
     )
