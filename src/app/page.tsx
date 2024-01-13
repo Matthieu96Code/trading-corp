4c1bff29b54202875f3cb57cafd40d416d6337e0
Matthieu96Code <senoumatthieu96@gmail.com>
2024-01-13 13:59:25 +0000

    Change sections order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,15 @@ import Header from "@/components/Header";
 import Products from "@/components/Products";
 import Faq from "@/components/Faq";
 import Footer from "@/components/Footer";
+import Training from "@/components/products/Training";
+import Automate from "@/components/products/Automate";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Automate />
+      <Training />
       <Products />
       <Faq />
       <Footer />
