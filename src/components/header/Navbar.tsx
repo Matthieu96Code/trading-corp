832a8d4cea4447c03d10e8b48ecee1aff3cc55b4
Matthieu96Code <senoumatthieu96@gmail.com>
2024-01-15 11:58:33 +0000

    Add proper links

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,15 +1,44 @@
-import Link from "../../../node_modules/next/link"
+import Link from "next/link";
 
 export default function Navbar() {
+  // const [navbarOpen, setNavbarOpen] = useState(false)
+
+  // const ToggleEvent = () => {
+    // setNavbarOpen(!navbarOpen)
+  // }
+
   return (
-    <nav className="bg-slate-900 flex row justify-between px-[15%] py-6 text-white" >
-      <h1>Trading<span>Corp</span></h1>
-      <ul className="flex row w-1/6 justify-between">
+    <nav className="bg-slate-900 flex row justify-between px-[9%] py-6 text-white" >
+      <h1 className="hover:text-rose-600">Trading<span>Corp</span></h1>
+      {/* <button onClick={() => ToggleEvent()} className="md:hidden hover:text-rose-600">Menu</button>
+      (navbarOpen) && (
+        <ul className="fixed">
+          <li>
+            <Link className="hover:text-rose-600" href="#services">Services</Link>
+          </li>
+          <li>
+            <Link className="hover:text-rose-600" href="#products">Products</Link>
+          </li>
+          <li>
+            <Link className="hover:text-rose-600" href="#exchange">Exchange</Link>
+          </li>
+          <li>
+            <Link className="hover:text-rose-600" href="#facs">Facs</Link>
+          </li>
+        </ul>
+      ) */}
+      <ul className="hidden md:flex gap-8 row justify-between">
         <li>
-            <Link href="#products">Products</Link>
+            <Link className="hover:text-rose-600" href="#services">Services</Link>
         </li>
         <li>
-            <Link href="#services">Services</Link>
+            <Link className="hover:text-rose-600" href="#products">Products</Link>
+        </li>
+        <li>
+            <Link className="hover:text-rose-600" href="#exchange">Exchange</Link>
+        </li>
+        <li>
+            <Link className="hover:text-rose-600" href="#facs">Facs</Link>
         </li>
       </ul>
     </nav>
