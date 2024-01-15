2095ece393a5225739b9ee5ae11504a7e62ec0ef
Matthieu96Code <senoumatthieu96@gmail.com>
2024-01-15 11:57:40 +0000

    Add id and update title

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,13 +1,11 @@
-import Training from "./products/Training"
-import Automate from "./products/Automate"
 import Robots from "./products/Robots"
 import Trade from "./products/Trade"
 
 export default function Products() {
     return (
-        <section className="flex flex-col">
+        <section className="flex flex-col" id="products">
             <div className="bg-gray-200 py-10 ">
-                <h2 className="my-4 text-center text-4xl">Products & services</h2>
+                <h2 className="my-4 text-center text-4xl">Products</h2>
                 <div className="my-4">
                     <h4 className="my-2 text-center text-2xl">To make money in trading, you don&apos;t need much.</h4>
                     <p className="text-center text px-2 md:px-24">Most traders believe that to be profitable in trading, it takes years of experience or memorizing every technical indicator and chart pattern. This belief is mistaken because all you really need is a solid trading system that supports and guides your decision-making in the markets.</p>
