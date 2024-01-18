f49a99516a0eaf7343c953efe5de7e18974ea967
Matthieu96Code <senoumatthieu96@gmail.com>
2024-01-18 11:52:00 +0000

    Add basic mobile menu

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,46 +1,58 @@
-import Link from "next/link";
+"use client"
+
+import Link from "next/link"
+import { useState } from "react"
 
 export default function Navbar() {
-  // const [navbarOpen, setNavbarOpen] = useState(false)
+  const [navbarOpen, setNavbarOpen] = useState(false)
 
-  // const ToggleEvent = () => {
-    // setNavbarOpen(!navbarOpen)
-  // }
+  const ToggleEvent = () => {
+    setNavbarOpen(!navbarOpen)
+  }
 
   return (
-    <nav className="bg-slate-900 flex row justify-between px-[9%] py-6 text-white" >
-      <h1 className="hover:text-rose-600">Trading<span>Corp</span></h1>
-      {/* <button onClick={() => ToggleEvent()} className="md:hidden hover:text-rose-600">Menu</button>
-      (navbarOpen) && (
-        <ul className="fixed">
+    <>
+      {(navbarOpen) && (
+        <nav  className="fixed top-0 left-0 right-0 w-screen h-screen bg-slate-900 text-white">
+          <div className="flex justify-between px-[9%] py-6">
+            <h1 className="hover:text-rose-600">Trading<span>Corp</span></h1>
+            <button onClick={() => ToggleEvent()} className="md:hidden hover:text-rose-600">Menu</button>
+          </div>
+          <ul onClick={() => ToggleEvent()} className="mt-10 flex flex-col items-center gap-10">
+            <li>
+              <Link className="hover:text-rose-600" href="#services">Services</Link>
+            </li>
+            <li>
+              <Link className="hover:text-rose-600" href="#products">Products</Link>
+            </li>
+            <li>
+              <Link className="hover:text-rose-600" href="#exchange">Exchange</Link>
+            </li>
+            <li>
+              <Link className="hover:text-rose-600" href="#facs">Facs</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+      <nav className="bg-slate-900 flex row justify-between px-[9%] py-6 text-white" >
+        <h1 className="hover:text-rose-600">Trading<span>Corp</span></h1>
+        <button onClick={() => ToggleEvent()} className="md:hidden hover:text-rose-600">Menu</button>
+        
+        <ul className="hidden md:flex gap-8 row justify-between">
           <li>
-            <Link className="hover:text-rose-600" href="#services">Services</Link>
+              <Link className="hover:text-rose-600" href="#services">Services</Link>
           </li>
           <li>
-            <Link className="hover:text-rose-600" href="#products">Products</Link>
+              <Link className="hover:text-rose-600" href="#products">Products</Link>
           </li>
           <li>
-            <Link className="hover:text-rose-600" href="#exchange">Exchange</Link>
+              <Link className="hover:text-rose-600" href="#exchange">Exchange</Link>
           </li>
           <li>
-            <Link className="hover:text-rose-600" href="#facs">Facs</Link>
+              <Link className="hover:text-rose-600" href="#facs">Facs</Link>
           </li>
         </ul>
-      ) */}
-      <ul className="hidden md:flex gap-8 row justify-between">
-        <li>
-            <Link className="hover:text-rose-600" href="#services">Services</Link>
-        </li>
-        <li>
-            <Link className="hover:text-rose-600" href="#products">Products</Link>
-        </li>
-        <li>
-            <Link className="hover:text-rose-600" href="#exchange">Exchange</Link>
-        </li>
-        <li>
-            <Link className="hover:text-rose-600" href="#facs">Facs</Link>
-        </li>
-      </ul>
-    </nav>
+      </nav>
+    </>
   )
 }
